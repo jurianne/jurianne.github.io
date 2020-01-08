096af1905e33bb6f8acd7357406424313d09b76f
juri <jurifujii0610@gmail.com>
2020-01-08 11:40:38 +0900

    add renewal version

--- scripts/main.js
+++ scripts/main.js
@@ -1,58 +1,55 @@
-jQuery(document).ready(function($){
-    var isLateralNavAnimating = false;
-    
-    //open/close lateral navigation
-    $('.cd-nav-trigger').on('click', function(event){
-        event.preventDefault();
-        //stop if nav animation is running 
-        if( !isLateralNavAnimating ) {
-            if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
-            
-            $('body').toggleClass('navigation-is-open');
-            $('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-                //animation is over
-                isLateralNavAnimating = false;
-            });
-        }
-    });
+function slideSwitch() {
+	var $active = $('.slideshow li.active');
 
+	if ($active.length == 0) $active = $('.slideshow li:last');
+
+	var $next = $active.next().length ? $active.next()
+		: $('.slideshow li:first');
+
+	$active.addClass('last-active');
+
+	$next.css({ opacity: 0.0 })
+		.addClass('active')
+		.animate({ opacity: 1.0 }, 1000, function () {
+			$active.removeClass('active last-active');
+		});
+}
+
+$(function () {
+	setInterval("slideSwitch()", 2100);
 });
 
 
-! function(a) {
-    "use strict";
-    a(document).ready(function() {
-        function b() {
-            window.location = i
-        }
-        var e = a(".site-title"),
-            f = a("body");
-        setTimeout(function() {
-            f.addClass("loaded")
-        }, 150);
-        var g = a(".project-assets").children("div");
-        a.each(g, function(b, c) {
-            var d = a(this);
-            d.imagesLoaded(function() {
-                setTimeout(function() {
-                    d.addClass("loaded")
-                }, 50 + 150 * b)
-            })
-        });
-        var h = a(".projects-feed .project");
-        a.each(h, function(b, c) {
-            var d = a(this);
-            d.imagesLoaded(function() {
-                setTimeout(function() {
-                    d.addClass("loaded")
-                }, 50 + 50 * b)
-            })
-        });
-        var i;
-        a("a").on("click", function(a) {
-            return "" == this.href || null == this.href ? void a.preventDefault() : void(-1 == this.href.indexOf("#") && -1 == this.href.indexOf("mailto:") && -1 == this.href.indexOf("javascript:") && "_blank" != this.target && (a.preventDefault(), i = this.href, f.removeClass("loaded"), setTimeout(b, 250)))
-        })
-    })
-}(window.jQuery);
+$(function () {
 
- 
+	/*
+		Drawer Menu
+	------------------------------------------------*/
+	$('.js-button-hamburger').click(function () {
+		$('body').toggleClass('is-active-drawer');
+
+		if ($(this).attr('aria-expanded') == 'false') {
+			$(this).attr('aria-expanded', true);
+			// スクロールを止めるクラスを追加
+			$('body').addClass('stop-scroll');
+		} else {
+			$(this).attr('aria-expanded', false);
+			// スクロールを止めるクラスを削除
+			$('body').removeClass('stop-scroll');
+		}
+	});
+
+});
+
+  /* ==============================
+    Accordion
+  ============================== */
+  $(function () {
+    $('.js-hidden-item').hide();
+
+    $('.js-button').click(function () {
+      // イベントが発生した要素の次の要素
+	  $('.js-hidden-item').slideDown(300);
+	  $(this).hide();
+    });
+  });

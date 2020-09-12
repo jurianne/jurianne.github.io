06a8b1943e6f51564bf20b72faec6baf781b3d57
juri <jurifujii0610@gmail.com>
2020-09-13 01:05:31 +0900

    fix

--- test/script/camera.js
+++ test/script/camera.js
@@ -1,18 +1,8 @@
-var player = document.getElementById('player'); 
-  var snapshotCanvas = document.getElementById('snapshot');
-  var captureButton = document.getElementById('capture');
+var player = document.getElementById('player');
 
   var handleSuccess = function(stream) {
-    // Attach the video stream to the video element and autoplay.
     player.srcObject = stream;
   };
 
-  captureButton.addEventListener('click', function() {
-    var context = snapshot.getContext('2d');
-    // Draw the video frame to the canvas.
-    context.drawImage(player, 0, 0, snapshotCanvas.width, 
-        snapshotCanvas.height);
-  });
-
-  navigator.mediaDevices.getUserMedia({video: true})
-      .then(handleSuccess);
+  navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+      .then(handleSuccess)

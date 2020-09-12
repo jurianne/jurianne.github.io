0a0b73ad9bcecdbcd362590cdc9844ae3b8391f2
juri <jurifujii0610@gmail.com>
2020-09-13 02:34:00 +0900

    fix

--- test/script/camera.js
+++ test/script/camera.js
@@ -1,44 +1,8 @@
-const cameraSize = { w: 360, h: 240 };
-const canvasSize = { w: 360, h: 240 };
-const resolution = { w: 1080, h: 720 };
-let video;
-let media;
-let canvas;
-let canvasCtx;
+var camera = document.getElementById('camera');
+var frame = document.getElementById('frame');
 
-// video要素をつくる
-video          = document.createElement('video');
-video.id       = 'video';
-video.width    = cameraSize.w;
-video.height   = cameraSize.h;
-video.autoplay = true;
-document.getElementById('videoPreview').appendChild(video);
-
-// video要素にWebカメラの映像を表示させる
-media = navigator.mediaDevices.getUserMedia({
-  audio: false,
-  video: {
-    width: { ideal: 640 },
-    height: { ideal: 300 }
-  }
-}).then(function(stream) {
-  video.srcObject = stream;
+camera.addEventListener('change', function(e) {
+  var file = e.target.files[0]; 
+  // Do something with the image file.
+  frame.src = URL.createObjectURL(file);
 });
-
-// canvas要素をつくる
-canvas        = document.createElement('canvas');
-canvas.id     = 'canvas';
-canvas.width  = canvasSize.w;
-canvas.height = canvasSize.h;
-document.getElementById('canvasPreview').appendChild(canvas);
-
-// コンテキストを取得する
-canvasCtx = canvas.getContext('2d');
-
-// video要素の映像をcanvasに描画する
-_canvasUpdate();
-
-function _canvasUpdate() {
-  canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
-  requestAnimationFrame(_canvasUpdate);
-};

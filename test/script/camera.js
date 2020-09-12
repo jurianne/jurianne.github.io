ea7aaa59a23829d368030102bf382339565cbb8a
juri <jurifujii0610@gmail.com>
2020-09-13 01:56:08 +0900

    fix

--- test/script/camera.js
+++ test/script/camera.js
@@ -1,8 +1,23 @@
-var player = document.getElementById('player');
+const cameraSize = { w: 360, h: 240 };
+const resolution = { w: 1080, h: 720 };
+let video;
+let media;
 
-  var handleSuccess = function(stream) {
-    player.srcObject = stream;
-  };
+// video要素をつくる
+video          = document.createElement('video');
+video.id       = 'video';
+video.width    = cameraSize.w;
+video.height   = cameraSize.h;
+video.autoplay = true;
+document.getElementById('videoPreview').appendChild(video);
 
-  navigator.mediaDevices.getUserMedia({video: true})
-      .then(handleSuccess);
+// video要素にWebカメラの映像を表示させる
+media = navigator.mediaDevices.getUserMedia({
+  audio: false,
+  video: {
+    width: { ideal: resolution.w },
+    height: { ideal: resolution.h }
+  }
+}).then(function(stream) {
+  video.srcObject = stream;
+});
